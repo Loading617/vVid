eb7919e8e5f2eb2444bd10210bf2aa9d8a824045
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-30 13:31:25 -0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,70 +1,106 @@
+using LibVLCSharp.Shared;
 using Microsoft.Win32;
 using System;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace vVid
 {
     public partial class MainWindow : Window
     {
-        private DispatcherTimer timer;
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+        private DispatcherTimer _timer;
 
         public MainWindow()
         {
             InitializeComponent();
-            mediaElement.MediaOpened += MediaElement_MediaOpened;
-            mediaElement.MediaEnded += MediaElement_MediaEnded;
+            Core.Initialize();
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(500);
-            timer.Tick += Timer_Tick;
+            _libVLC = new LibVLC("--no-xlib");
+            _mediaPlayer = new MediaPlayer(_libVLC);
+            videoView.MediaPlayer = _mediaPlayer;
+
+            _mediaPlayer.LengthChanged += MediaPlayer_LengthChanged;
+            _mediaPlayer.TimeChanged += MediaPlayer_TimeChanged;
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(500)
+            };
+            _timer.Tick += (s, e) =>
+            {
+                if (_mediaPlayer?.IsPlaying == true)
+                {
+                    seekSlider.Value = _mediaPlayer.Time;
+                }
+            };
         }
 
-        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        private void OpenVideo_Click(object sender, RoutedEventArgs e)
         {
-            var openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Video files (*.mp4;*.wmv)|*.mp4;*.wmv|All files (*.*)|*.*";
-
-            if (openFileDialog.ShowDialog() == true)
+            var dialog = new OpenFileDialog
             {
-                mediaElement.Source = new Uri(openFileDialog.FileName);
-                mediaElement.Play();
-                timer.Start();
+                Filter = "Video files|*.mp4;*.mkv;*.avi;*.mov|All files|*.*"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                _mediaPlayer.Media = new Media(_libVLC, new Uri(dialog.FileName));
+                _mediaPlayer.Play();
+                _timer.Start();
             }
         }
 
-        private void BtnPlay_Click(object sender, RoutedEventArgs e) => mediaElement.Play();
-        private void BtnPause_Click(object sender, RoutedEventArgs e) => mediaElement.Pause();
-        private void BtnStop_Click(object sender, RoutedEventArgs e) => mediaElement.Stop();
-
-        private void MediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        private void OpenSubtitle_Click(object sender, RoutedEventArgs e)
         {
-            seekSlider.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalSeconds;
-        }
-
-        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            timer.Stop();
-            mediaElement.Stop();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            if (mediaElement.NaturalDuration.HasTimeSpan)
+            var dialog = new OpenFileDialog
             {
-                seekSlider.Value = mediaElement.Position.TotalSeconds;
-                txtPosition.Text = $"{mediaElement.Position:mm\\:ss} / {mediaElement.NaturalDuration.TimeSpan:mm\\:ss}";
+                Filter = "Subtitles|*.srt;*.sub;*.ass"
+            };
+            if (dialog.ShowDialog() == true && _mediaPlayer.IsPlaying)
+            {
+                _mediaPlayer.AddSlave(MediaSlaveType.Subtitle, dialog.FileName, true);
             }
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e) => _mediaPlayer.Play();
+        private void Pause_Click(object sender, RoutedEventArgs e) => _mediaPlayer.Pause();
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Stop();
+            _timer.Stop();
+            seekSlider.Value = 0;
+        }
+
+        private void MediaPlayer_LengthChanged(object sender, MediaPlayerLengthChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() => seekSlider.Maximum = e.Length);
+        }
+
+        private void MediaPlayer_TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() => seekSlider.Value = e.Time);
         }
 
         private void SeekSlider_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            mediaElement.Position = TimeSpan.FromSeconds(seekSlider.Value);
+            if (_mediaPlayer?.IsPlaying == true)
+            {
+                _mediaPlayer.Time = (long)seekSlider.Value;
+            }
         }
 
         private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            mediaElement.Volume = volumeSlider.Value;
+            _mediaPlayer.Volume = (int)e.NewValue;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _mediaPlayer?.Dispose();
+            _libVLC?.Dispose();
         }
     }
 }
